3e99220d450db5db4722328bfcfd23967720ad9a
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-09-28 09:17:30 -0700

    Basic C program and Makefile
    Learnt how to write and break C program, understood how a C file in compiled, and how to make a custom Makefile to automate stuff

---
+++ learning_c/ex1_basic.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+/* Basic C program */
+
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+
+  // Printing now
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
+}
